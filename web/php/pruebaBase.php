5a895584516d0c3c5be80310c4065b1c10b3a705
DESKTOP-R07FLRE\Alberto <rodriguez.jesus0794@gmail.com>
2017-05-25 00:34:21 -0500

    prueba base de datos

--- web/php/pruebaBase.php
+++ web/php/pruebaBase.php
@@ -2,23 +2,37 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+	   table,th,td{
+	   	border-collapse: collapse;
+		border:1px solid black;
+	   }
+
+
+		
+
+	</style>
 </head>
 <body>
 <?php
 include 'consultasDocente.php';
 include 'conexionDB.php';
 $resultado=mysqli_query($conexion,$consulta);
-
-
+ 
+echo "<table>";
+   echo "<tr><td>ID</td><td>nombre</td><td>Apellido1</td><td>Apellido2</td><td>Estatus</td></tr>";
 while ($fila=mysqli_fetch_row($resultado)) {
+	echo"<tr>";
 	for ($i=0; $i <count($fila) ; $i++) { 
+	echo "<td>";
 	echo $fila[$i];
+	echo "</td>";	
     }
     echo "<br>";
-
+    echo"</tr>";
 }
  mysqli_close($conexion);
-
+echo "</table>";
 ?>
 
 
